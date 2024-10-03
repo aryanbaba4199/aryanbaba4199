073c7cf15f6b2900ce3571fa84ae4ac782be6d47
Raushan Kumar <aryanbaba4199@gmail.com>
2024-10-03 21:25:08 +0530

    Update ExperienceData.jsx

--- constants/ExperienceData.jsx
+++ constants/ExperienceData.jsx
@@ -1,20 +1,38 @@
 export const ExperienceData = [
   {
-    companyName: "FreeLancer",
-    location: "Remote",
-    role: "Full Stack Developer | MERN Stack Developer",
-    fromTo : "From 01 October 2023",
+    companyName: "BPR Network",
+    location: "Patna, Bihar",
+    role: "React | Next Js Developer",
+    fromTo : "From 01 June 2024 to 1 August 2024",
 
     
     side: "left",
   },
   {
-    companyName: "Riya Technology",
+    companyName: "FreeLancer",
+    location: "Remote",
+    role: "Full Stack Developer | MERN Stack Developer",
+    fromTo : "Working Here",
+
+    
+    side: "right",
+  },
+  {
+    companyName: "Akchhat Films Production",
     location: "Patna, Bihar",
+    role: "Full Stack Developer | MERN Stack Developer",
+    fromTo : "From 01 December 2023 to 31-05-2024",
+
+    
+    side: "left",
+  },
+  {
+    companyName: "Websofto Technologies",
+    location: "Assam",
     role: "MERN Stack Developer",
 
     fromTo:
-     "From 01 Feb 2022 to 31 August 2023",
+     "From  March 2022 to August 2023",
     side: "right",
   },
   
